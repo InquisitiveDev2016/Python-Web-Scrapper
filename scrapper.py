03b49c5fcb6342462327240ebdd2cdce2274de36
InquisitiveDev2016 <alihaider2011@live.ca>
2019-12-22 23:08:58 -0500

    Create scrapper.py

---
+++ scrapper.py
@@ -0,0 +1,30 @@
+import urllib.request
+from bs4 import BeautifulSoup as soup
+import requests
+
+url = 'https://unsplash.com/s/photos/download'
+
+
+
+def download_imgs(url, amountOfImgs):
+    req=requests.get(url)
+    html=req.text
+    #parsing the html from the url
+    page_soup = soup(html, "html.parser")
+    images = [img for img in page_soup.findAll('img')]
+    counter = 0
+    #compiling the unicode list of image links
+    image_links = [each.get('src') for each in images]
+
+    for each in image_links:
+        if(counter <= amountOfImgs):
+            filename = each.split('/')[-1]
+            urllib.request.urlretrieve(each, filename)
+            counter += 1
+        else:
+            return image_links
+
+
+
+
+print(download_imgs(url, 5))
